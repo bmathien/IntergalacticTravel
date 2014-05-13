da50dcfc8d65bfa245593f98cb4d92a5d389e310
Brian Mathien <bmathien@barrington220.org>
2014-05-13 10:25:28 -0500

    Initial Commit

---
+++ IntergalacticTravel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IntergalacticTravel
+//
+//  Created by teacher on 5/13/14.
+//  Copyright (c) 2014 Mathien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
